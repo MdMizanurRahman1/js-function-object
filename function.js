ce87c9db88029cf62bd01b7330b1720bcc9373d5
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-01-30 00:44:36 +0200

    function practiced

---
+++ function.js
@@ -0,0 +1,9 @@
+function startFan() {
+    console.log('please turn off the fan')
+}
+startFan();
+
+function rentAmount(money) {
+    console.log('give me some onions and singara')
+}
+rentAmount(20);
---
+++ parameter.js
@@ -0,0 +1,18 @@
+function loveDil(honey) {
+    console.log('I love you janu')
+    console.log(honey)
+}
+var mon = 300;
+/* loveDil(mon); */
+
+function add(num1, num2) {
+    console.log('I m going to market:', num1, num2)
+}
+/* add(100, 20); */
+
+function sum(a, b, c, d, e) {
+    var total = a + b + c + d + e;
+    var average = total / 5;
+    console.log(average);
+}
+sum(23, 45, 67, 25, 90);
